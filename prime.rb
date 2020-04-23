b02c4f9d91e8df24fb3f50d9b319c1172523f1fa
Daniel Vo <daniiielvo@gmail.com>
2020-04-23 21:23:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(number)
-  return false if number < 2 
- return (2..number-1).to_a.none? do |possible_divider|
+   false if number < 2 
+  (2..number-1).to_a.none? do |possible_divider|
     number % possible_divider == 0 
   end 
   
