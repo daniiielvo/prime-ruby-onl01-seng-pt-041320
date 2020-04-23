9b329f71eb877fc3be292825d48667c2cf3757dc
Daniel Vo <daniiielvo@gmail.com>
2020-04-23 21:19:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(number)
   false if number < 2 
- (2..number-1).to_a.all? do |possible_divider|
+ return (2..number-1).to_a.all? do |possible_divider|
     possible_divider % number != 0 
   end 
- false    
+  
 end
